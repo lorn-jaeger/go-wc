b49ff16fd56c5e0920b037513300e7f0cf80c80e
Lorn Jaeger <mail@lorn.us>
2025-01-08 15:37:20 -0700

    updated code to include word count and flags. the program now also taked input from stdin. there is a little left to do to match wc formatting exactly

--- go-wc.go
+++ go-wc.go
@@ -1,73 +1,132 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"os"
+	"unicode"
 )
 
-func CountBytes(data string) int {
-	return len(data)
-}
+func CountLines(file []byte) int {
+	var count int
 
-func CountChars(data string) int {
-	return len([]rune(data))
-}
-
-func CountLines(data string) int {
-	const newline byte = '\n'
-	count := 0
-
-	for i := range len(data) {
-		if data[i] == newline {
+	for _, byte := range file {
+		if byte == '\n' {
 			count++
 		}
 	}
-	if data[len(data)-1] != newline {
-		count++
-	}
+
 	return count
 }
 
-func Delimeter(b byte) bool {
-	const space byte = ' '
-	const newline byte = '\n'
-
-	if b == space || b == newline {
-		return true
-	}
-	return false
-}
-
-func CountWords(data string) int {
-	const space byte = ' '
-	const newline byte = '\n'
-	count := 0
-
+func CountWords(file []byte) int {
+	var count int
 	insideWord := false
-	for i := range len(data) {
-		if Delimeter(data[i]) && insideWord == false {
-			insideWord = true
-			count++
-		}
 
-		if Delimeter(data[i]) {
-			insideWord = false
+	for _, b := range file {
+		if unicode.IsSpace(rune(b)) {
+			if insideWord {
+				insideWord = false
+			}
+		} else {
+			if !insideWord {
+				insideWord = true
+				count++
+			}
 		}
 	}
-
 	return count
-
 }
 
-func main() {
-	data, err := os.ReadFile("test.txt")
+func CountChars(file []byte) int {
+	return len([]rune(string(file)))
+}
+
+func CountBytes(file []byte) int {
+	return len(file)
+}
+
+func ReadFile(filename string) []byte {
+	file, err := os.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
+	return file
+}
 
-	fmt.Println(CountBytes(string(data)))
-	fmt.Println(CountChars(string(data)))
-	fmt.Println(CountLines(string(data)))
-	fmt.Println(CountWords(string(data)))
+func ReadStdin() []byte {
+	stdin, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+	return stdin
+}
+
+func FmtResults(results []int, filename string) string {
+	maxWidth := 0
+	for _, result := range results {
+		width := len(fmt.Sprint("%d", result))
+		if width > maxWidth {
+			maxWidth = width
+		}
+	}
+
+	output := ""
+
+	for _, result := range results {
+		output += fmt.Sprintf("%*d", maxWidth-1, result)
+	}
+
+	if filename != "" {
+		output += " " + filename
+	}
+
+	return output
+}
+
+func main() {
+	var filename string
+	var file []byte
+	var results []int
+
+	lineFlag := flag.Bool("l", false, "Count lines in file")
+	wordFlag := flag.Bool("w", false, "Count words in file")
+	charFlag := flag.Bool("m", false, "Count chars in file")
+	byteFlag := flag.Bool("c", false, "Count bytes in file")
+
+	flag.Parse()
+	args := flag.Args()
+
+	if !*lineFlag && !*wordFlag && !*charFlag && !*byteFlag {
+		*lineFlag, *wordFlag, *byteFlag = true, true, true
+	}
+
+	if len(args) > 0 {
+		filename = args[0]
+		file = ReadFile(filename)
+
+	} else {
+		filename = ""
+		file = ReadStdin()
+	}
+
+	if *lineFlag {
+		results = append(results, CountLines(file))
+	}
+
+	if *wordFlag {
+		results = append(results, CountWords(file))
+	}
+
+	if *charFlag {
+		results = append(results, CountChars(file))
+	}
+
+	if *byteFlag {
+		results = append(results, CountBytes(file))
+	}
+
+	fmt.Println(FmtResults(results, filename))
 
 }
